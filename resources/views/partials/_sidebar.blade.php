6fcc9b126d2f05a261e41630fc52cc45f0835c4d
mrayor <adesanyayokunle@gmail.com>
2018-01-14 11:50:02 +0100

    added _sidebar partials
    Make the code cleaner

---
+++ resources/views/partials/_sidebar.blade.php
@@ -0,0 +1,131 @@
+<div class="sidebars">
+	<div class="sidebars-wrap">
+		<div class="sidebar">
+			<div class="widget widget_search">
+				<h3 class="widget-title">Search</h3>
+				<form class="search-form">
+					<input type="search" class="search-field" placeholder="Type & Hit Enter...">
+					<input type="submit" class="search-submit">
+				</form>
+			</div>
+			<!-- /.widget_search -->
+
+			<div class="widget widget_categories">
+				<h3 class="widget-title">Categories</h3>
+				<ul>
+					<li>
+						<a href="#">Chicken Tinga Nachos (9)</a>
+					</li>
+					<li>
+						<a href="#">Sweet and Sour Pork Chops (9)</a>
+					</li>
+					<li>
+						<a href="#">Green Chile And Chicken Stew (9)</a>
+					</li>
+					<li>
+						<a href="#">Pecan Pie Macarons (9)</a>
+					</li>
+					<li>
+						<a href="#">Brazilian Burger With Egg (9)</a>
+					</li>
+					<li>
+						<a href="#">Sweet and Sour Pork Chops (9)</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /.widget_categories -->
+
+			<div class="widget widget_recent_entries clearfix">
+				<h3 class="widget-title">Recent Post</h3>
+				<ul class="recent-post clearfix">
+					<li>
+						<div class="thumb">
+							<img src="/img/blog_img/blog-thumbnail.jpg" alt="image">
+						</div>
+						<div class="text">
+							<h4>
+								<a href="#">Post with couple photos inside</a>
+							</h4>
+							<p>On 13 Nov 2015</p>
+						</div>
+					</li>
+
+					<li>
+						<div class="thumb">
+							<img src="/img/blog_img/blog-thumbnail.jpg" alt="image">
+						</div>
+						<div class="text">
+							<h4>
+								<a href="#">Post with couple photos inside</a>
+							</h4>
+							<p>On 13 Nov 2015</p>
+						</div>
+					</li>
+
+					<li>
+						<div class="thumb">
+							<img src="/img/blog_img/blog-thumbnail.jpg" alt="image">
+						</div>
+						<div class="text">
+							<h4>
+								<a href="#">Post with couple photos inside</a>
+							</h4>
+							<p>On 13 Nov 2015</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<!-- /.widget_recent_entries -->
+
+			<div class="widget widget_latest_tweets">
+				<h3 class="widget-title">Latest Tweets</h3>
+				<ul>
+					<li>
+						<p>Portfolio Page Creation: </p>
+						<p>
+							<a href="#">https://t.co/TN773odoa</a>@YouTube</p>
+						<p>5 days ago</p>
+					</li>
+
+					<li>
+						<p>Portfolio Page Creation: </p>
+						<p>
+							<a href="#">https://t.co/TN773odoa</a> @YouTube</p>
+						<p>5 days ago</p>
+					</li>
+
+					<li>
+						<p>Portfolio Page Creation: </p>
+						<p>
+							<a href="#">https://t.co/TN773odoa</a> @YouTube</p>
+						<p>5 days ago</p>
+					</li>
+
+
+				</ul>
+			</div>
+			<!-- /.widget_latest_tweets -->
+
+			<div class="widget widget_tag">
+				<h3 class="widget-title">Popular Tags</h3>
+				<div class="tag-list">
+					<a class="active" href="#">example, </a>
+					<a href="#">Gallery, </a>
+					<a href="#">Image, </a>
+					<a href="#">quote, </a>
+					<a href="#">tag, </a>
+					<a href="#">Video,</a>
+					<a href="#">PSD Teplates,</a>
+					<a href="#">Business, </a>
+					<a href="#">Portfolio, </a>
+					<a href="#">Construction, </a>
+					<a href="#">One Page, </a>
+				</div>
+			</div>
+			<!-- /.widget_tag -->
+		</div>
+		<!-- .sidebar -->
+	</div>
+	<!-- /.sidebars-wrap -->
+</div>
+<!-- /.sidebars -->
