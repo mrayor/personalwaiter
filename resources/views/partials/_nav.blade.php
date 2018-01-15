2f639d09cbccc7d54cb7ed7e1eaf95bc42cc0120
mrayor <adesanyayokunle@gmail.com>
2018-01-15 23:41:51 +0100

    Auth guest
    Change menu for login users

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -16,11 +16,18 @@ You are using an <strong>outdated</strong> browser. Please <a href="http://brows
     <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
             <ul class="nav navbar-nav">
-                <li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a title="About" href="/about">About</a></li>
-                <li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a title="Careers" href="/career">Careers</a></li>
-                <li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-21"><a title="Blog" href="/blog">Blog</a></li>
-                <li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a title="Contact" href="/contact">Contact</a></li>
-                <li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a title="Order" href="http://order.personalwaiter.dev">Order</a></li>
+                @if (Auth::guest())
+                    <li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a title="About" href="/about">About</a></li>
+                    <li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a title="Careers" href="/career">Careers</a></li>
+                    <li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-21"><a title="Blog" href="/blog">Blog</a></li>
+                    <li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a title="Contact" href="/contact">Contact</a></li>
+                    <li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a title="Order" href="http://order.personalwaiter.dev">Order</a></li>
+                @else
+                    <li id="menu-item-22" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-22"><a title="Post" href="/Posts">Posts</a></li>
+                    <li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-20"><a title="Categories" href="/categories">Categories</a></li>
+                    <li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-21"><a title="Tags" href="/tags">Tags</a></li>
+                    <li id="menu-item-29" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-29"><a title="Logout" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a><form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form></li>
+                @endif
             </ul>
         </div>
     </div>
