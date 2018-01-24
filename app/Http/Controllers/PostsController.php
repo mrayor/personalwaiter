2e4ef5dfd3676a1221dd0bdf31c0de78c2f66728
mrayor <adesanyayokunle@gmail.com>
2018-01-24 10:15:19 +0100

    Html Purifier
    Security on WYSIWYG

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,6 +7,7 @@ use App\Post;
 use App\Tag;
 use App\Category;
 use Session;
+use Purifier;
 
 class PostsController extends Controller
 {
@@ -64,7 +65,7 @@ class PostsController extends Controller
         $post->title = $request->title;
         $post->slug = $request->slug;
         $post->category_id = $request->category_id;
-        $post->body = $request->body;
+        $post->body = Purifier::clean($request->body);
         //save post in table
         $post->save();
 
@@ -155,7 +156,7 @@ class PostsController extends Controller
         $post->title = $request->input('title');
         $post->slug = $request->input('slug');
         $post->category_id = $request->input('category_id');
-        $post->body = $request->input('body');
+        $post->body = Purifier::clean($request->input('body'));
         $post->save();
 
         if (isset($request->tags)){
