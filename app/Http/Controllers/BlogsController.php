75d67bbe8fa99008b88681e6bbac900687d858e8
mrayor <adesanyayokunle@gmail.com>
2018-01-30 23:50:21 +0100

    fixed bug
    fixed single page bug

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -12,10 +12,11 @@ class BlogsController extends Controller
     public function single($slug) {
     //fetch database based on slug
     $post = Post::where('slug', '=', $slug)->first();
+    $posts = Post::orderBy('created_at', 'desc')->paginate(2);
     $tags = Tag::all();
     $categories  = Category::all();
  
     // return the view and pass in the post object 
-    return view('blog.single')->withPost($post)->withCategories($categories)->withTags($tags);
+    return view('blog.single')->withPost($post)->withCategories($categories)->withTags($tags)->withPosts($posts);
     }
 }
