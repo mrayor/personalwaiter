1ef2010302fdcc9a627ac55ad3b02830f6dad699
Adesanya Ayokunle <adesanya_ayokunle@yahoo.com>
2018-03-18 18:15:13 +0100

    Cleaning Up
    Commented out some js files

--- resources/views/partials/_scripts.blade.php
+++ resources/views/partials/_scripts.blade.php
@@ -1,13 +1,13 @@
-<script type='text/javascript' src="{{asset('js/jquery/jquery.js')}}"></script>
-<script type='text/javascript' src="{{asset('js/jquery/jquery-migrate.min.js')}}"></script>
-<script type='text/javascript' src="{{asset('js/jquery/jquery.peity.min.js')}}"></script>
-<script type='text/javascript' src="{{asset('js/initial.min.js')}}"></script>
-<script type='text/javascript' src="{{asset('js/watch.min.js')}}"></script>
-<script type='text/javascript' src="{{asset('js/jquery/jquery.form.min.js')}}"></script>
+<script type='text/javascript' src="{{asset('js/jquery/jquery.js')}}"></script> 
+{{--  <script type='text/javascript' src="{{asset('js/jquery/jquery-migrate.min.js')}}"></script>  --}}
+{{--  <script type='text/javascript' src="{{asset('js/jquery/jquery.peity.min.js')}}"></script>  --}}
+{{--  <script type='text/javascript' src="{{asset('js/initial.min.js')}}"></script>  --}}
+{{--  <script type='text/javascript' src="{{asset('js/watch.min.js')}}"></script>  --}}
+{{--  <script type='text/javascript' src="{{asset('js/jquery/jquery.form.min.js')}}"></script>  --}}
 <script type='text/javascript' src="{{asset('js/anspress.min.js')}}"></script>
-<script type='text/javascript' src="{{asset('js/anspress-theme.min.js')}}"></script>
-<script type='text/javascript' src="{{asset('js/mention.js')}}"></script>     
-<script type='text/javascript' src="{{asset('js/modernizr-2.8.3.min.js')}}"></script>
+{{--  <script type='text/javascript' src="{{asset('js/anspress-theme.min.js')}}"></script>  --}}
+{{--  <script type='text/javascript' src="{{asset('js/mention.js')}}"></script>       --}}
+{{--  <script type='text/javascript' src="{{asset('js/modernizr-2.8.3.min.js')}}"></script>  --}}
 <script type='text/javascript' src="{{asset('js/bootstrap.min.js')}}"></script>
 <script type='text/javascript' src="{{asset('js/bst.js')}}"></script>
 <script type='text/javascript' src="{{asset('js/parallax.js')}}"></script>
